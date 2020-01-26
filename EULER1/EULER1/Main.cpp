b0b04dc9444ac4c417025b9896afcec9746bafc0
PC-EN-BAS-PASCAL\lac900 <pascal.chayer@polymtl.ca>
2020-01-25 20:49:05 -0500

    Main for the execution

---
+++ EULER1/EULER1/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Solution1.h"
+#include "Solution2.h"
+#include "Solution3.h"
+
+
+
+using namespace std;
+
+int main() {
+	cout << Solution1() << endl;
+	cout << Solution2() << endl;
+	cout << Solution3(600851475143) << endl;
+	system("PAUSE");
+
+}
