988cc7b27044d15c7b9e493536183a3144086fe6
PC-EN-BAS-PASCAL\lac900 <pascal.chayer@polymtl.ca>
2020-01-25 21:05:45 -0500

    Start of question 4

---
+++ EULER1/EULER1/Solution4.cpp
@@ -0,0 +1,16 @@
+#include "Solution4.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Solution4()
+{
+	cout << "RESULTAT SOLUTION 4" << endl;
+
+	for (int i = 99; i >= 10; i--) {
+		for (int j = 99; j >= 10; j--) {
+			string s = "saas";
+		}
+	}
+}
---
+++ EULER1/EULER1/Solution4.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int Solution4();
