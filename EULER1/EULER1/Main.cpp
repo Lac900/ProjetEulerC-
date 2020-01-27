cf85ff15df4add13d66582cad184dd2fd1635313
lac900 <lac900@live.ca>
2020-01-27 16:01:56 -0500

    Solution question 4

--- EULER1/EULER1/Main.cpp
+++ EULER1/EULER1/Main.cpp
@@ -2,6 +2,7 @@
 #include "Solution1.h"
 #include "Solution2.h"
 #include "Solution3.h"
+#include "Solution4.h"
 
 
 
@@ -10,7 +11,8 @@ using namespace std;
 int main() {
 	cout << Solution1() << endl;
 	cout << Solution2() << endl;
-	cout << Solution3(600851475143) << endl;
+	//cout << Solution3(600851475143) << endl;
+	cout << Solution4() << endl;
 	system("PAUSE");
 
 }
--- EULER1/EULER1/Solution4.cpp
+++ EULER1/EULER1/Solution4.cpp
@@ -4,13 +4,40 @@
 
 using namespace std;
 
+bool compareInverse(string S1, string S2) {
+
+	for (int i = 0; i < S1.length(); i++) {
+		if (S1[i] != S2[S1.length() - 1 - i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool IsPalindrome(string value) {
+	int len = value.length();
+	if ( len % 2 ) {
+		return false;
+	}
+	string part1 = value.substr(0,(len / 2));
+	string part2 = value.substr((len / 2));
+	return compareInverse(part1, part2);
+}
+
 int Solution4()
 {
 	cout << "RESULTAT SOLUTION 4" << endl;
 
-	for (int i = 99; i >= 10; i--) {
-		for (int j = 99; j >= 10; j--) {
-			string s = "saas";
+	int result = 0;
+
+	for (int i = 999; i >= 100; i--) {
+		for (int j = 999; j >= 100; j--) {
+			int nbr = (i * j);
+			string number = to_string(nbr);
+			if (IsPalindrome(number)) {
+				result = result < nbr ? nbr : result;
+			}
 		}
 	}
+	return result;
 }
